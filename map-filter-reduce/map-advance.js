64cf1bf28e32a5b3d41779e1b3853cf0f6fcbf88
gajurelkshitiz <kshitiz.purbct042@gmail.com>
2025-09-03 13:00:27 +0545

    advance example of map-reduce

--- map-filter-reduce/map-advance.js
+++ map-filter-reduce/map-advance.js
@@ -5,6 +5,31 @@ const users = [
     { firstname: "deepika", lastName: "padukone", age: 26 },
 ]
 
+
+// map demonstration:
 const output = users.map(user => user.firstname + " " + user.lastName)
 
 console.log(output);
+
+
+// Filter demonstration:
+
+const value = users.filter(user => user.age === 26)
+console.log(value);
+
+
+// we need to show output like : { 26: 2, 75: 1, 50: 1}  [age: count]
+
+const countAge = users.reduce((acc, curr) => {
+    const age = curr.age;
+    if (age in acc) {
+        acc[age] += 1
+    }
+    else{
+        acc[age] = 1
+    }
+    return acc
+}, {})
+
+console.log(countAge);
+
